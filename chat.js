1f4d186e7710aa840df215b9ec1e42c88e8ac82b
smithkenny482 <119724953+smithkenny482@users.noreply.github.com>
2023-10-21 21:14:50 +0800

    test latest update

--- chat.js
+++ chat.js
@@ -2,7 +2,6 @@ class Connection {
     constructor(username) {
         this.username = username;
         this.message_handlers = [];
-        // this.socket = new WebSocket("ws://localhost:8000/chat");
         this.socket = new WebSocket("ws://web3-backend-the-sos-brigade.koyeb.app/chat");
 
         this.connected = false;
@@ -28,16 +27,22 @@ class Connection {
     add_handler(handler) {
         this.message_handlers.push(handler);
     }
+
+    disconnect() {
+        this.socket.close();
+    }
 }
 
-let username = prompt("Enter your nick");
 
-let connection = new Connection(username);
+
+
+let username;
+let connection;
+let wallet_connection_status = false;
 
 function messageHandler(username, message) {
     let chat = document.getElementsByClassName("chat-messages").item(0);
 
-    // create message elements
     let message_div = document.createElement("div");
     message_div.classList.add("chat-message");
 
@@ -56,8 +61,6 @@ function messageHandler(username, message) {
     chat.appendChild(message_div)
     scrollToBottom();
 }
-
-connection.add_handler(messageHandler)
 
 function scrollToBottom() {
     let chat = document.querySelector(".chat-container");
@@ -83,3 +86,81 @@ document.getElementById("chat-text-input").addEventListener("keyup", function(ev
     }
 });
 
+
+function toggleHeight() {
+    const container = document.querySelector('.chat-container-wrapper');
+    container.classList.toggle('minimized');
+}
+
+
+const connectWalletButton = document.getElementById("connectWallet");
+const walletStatus = document.getElementById("walletStatus");
+const popupContainer = document.getElementById("popupContainer");
+const popupMessage = document.getElementById("popupMessage");
+
+function connectWallet() {
+
+    if (wallet_connection_status == false){
+        console.log('connecting wallet')
+        if (window.solana && window.solana.isPhantom) {
+                window.solana.connect()
+                    .then(() => {
+                        const publicKey = window.solana.publicKey;
+                        const addressStr = publicKey.toString();
+                        const shortenedAddress = addressStr.substring(0, 4) + "..." + addressStr.slice(-4);
+
+                        username = shortenedAddress;
+                        connection = new Connection(username);
+                        wallet_connection_status = true;
+                        connection.add_handler(messageHandler)
+
+                        const container = document.querySelector('.overlay');
+                        container.style.display="none";
+                        connectWalletButton.textContent = `${shortenedAddress}`;
+                    })
+                    .catch(error => {
+                        console.error("Error connecting to Phantom wallet:", error);
+                    });
+            } else {
+                console.error("Phantom wallet not found. Please install it.");
+                popupContainer.style.display = "block";
+            }
+    }
+
+    else {
+        const container = document.querySelector('.wallet-more-wrapper');
+        
+        if (container.style.display == "none") {
+            container.style.display = "block";
+        } 
+        else {
+            container.style.display = "none";
+
+        }
+    }
+    
+}
+
+connectWalletButton.addEventListener("click", connectWallet);
+
+const disconnectWalletButton = document.getElementById("disconnectWallet");
+function disconnectWallet() {
+  if (window.solana && window.solana.isPhantom) {
+    window.solana.disconnect();
+    connection.disconnect();
+    connectWalletButton.textContent = "Connect Wallet";
+    wallet_connection_status = false;
+    const wallet_wrapper = document.querySelector('.wallet-more-wrapper');
+    wallet_wrapper.style.display = "none";
+
+    const overlay = document.querySelector('.overlay');
+    overlay.style.display="block";
+
+  }
+}
+
+disconnectWalletButton.addEventListener("click", disconnectWallet);
+
+closeButton.addEventListener("click", function() {
+    popupContainer.style.display = "none";
+});
